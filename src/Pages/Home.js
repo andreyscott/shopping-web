9043d36ea5c8bfb0cf28e62a52943438c5e5523c
andreyscott <andreyscott301@gmail.com>
2022-05-08 20:12:52 +0100

    made it work

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {RiUserAddFill} from 'react-icons/ri'
+import Hero from '../components/hero/Hero';
 
 
 export const Home = () => {
   return (
 <div className='w-full '>
-     <p className='text-center text-cyan-400 h-72 bg-green-400 border-4 rounded-lg'>
-      <h1 className='flex justify-center bg-slate-500  h-36 items-center '>Welcome to the React App <RiUserAddFill /> </h1>
-     </p>
+     <Hero />
      </div>
   )
 }
---
+++ src/components/hero/SliderContent.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import heroImg from "../../data";
+import {FiMoreVertical} from 'react-icons/fi';
+//import ButtonFill from '../ButtonFill';
+
+function SliderContent({ activeIndex, sliderImage }) {
+  return (
+    <section>
+      {heroImg.map((slide, index) => (
+        <div
+          key={index}
+          className={index === activeIndex ? "slides active" : "inactive"}
+        >
+          <img className="slide-image" src={slide.image} alt="" />
+          <h2 className="slide-title">{slide.text}</h2>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default SliderContent;
