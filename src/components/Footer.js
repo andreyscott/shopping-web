de875f3078b7a37713f130aeddaea989febcdd0e
andreyscott <andreyscott301@gmail.com>
2022-05-10 00:59:32 +0100

    made it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,14 +38,13 @@ export default function Example() {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                   <div className="flex items-center">
-                    <div className="flex-shrink-0">
+                    <Link to="/" className="flex-shrink-0">
                       <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                         alt="Workflow"
                       />
-                    </div>
-               
+               </Link>
                   </div>
                   <div className="flex items-center">
                   <div className="hidden md:block">
