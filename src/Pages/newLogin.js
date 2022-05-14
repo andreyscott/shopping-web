f220e035372e04bced7154523152a04f5b3406f7
andreyscott <andreyscott301@gmail.com>
2022-05-14 16:09:44 +0100

    done with registration page

--- src/Pages/newLogin.js
+++ src/Pages/newLogin.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signIn } from "../redux/action";
 import { useNavigate } from "react-router-dom";
+import { XIcon } from '@heroicons/react/outline'
+import {BsGoogle} from 'react-icons/bs'
 
 function Signup() {
   const [name, setName] = useState("");
@@ -33,44 +35,45 @@ function Signup() {
   };
 
   return (
-    <>
-      <button
-        type="button"
-        className="btn btn-outline-dark ms-2 "
-        onClick={(e) => setModal(true)}
-      >
-        <i className="fa fa-user-plus me-1"></i> Register
-      </button>
+    <div className="bg-green-400  flex  justify-center items-center content-center">
+      <div className="bg-white shadow-md rounded-xl px-8 pt-6 my-4 mb-4">
       {/* <!-- Modal --> */}
       {modal === true && (
         <>
-          <div className="formWrapper" onClick={() => setModal(false)} />
-          <div className="modal-content position-absolute ">
-            <div className="d-flex justify-content-end align-items-center mb-1 mt-4 me-4">
+          <div className=" justify-center items-center content-center" onClick={() => setModal(false)} />
+          <div className="modal-content  position-absolute  ">
+            <div className="flex justify-end m-1">
               <button
                 type="button"
                 className="btn-close"
                 onClick={(e) => setModal(false)}
-              >xxx</button>
+              >
+              <XIcon className="block h-6 w-6" aria-hidden="true" />
+              </button>
             </div>
             <div className="modal-body ps-5 pe-5 pt-0 pb-3">
               <h5
-                className="modal-title login-header text-start mb-5"
+                className=" leading-normal text-4xl text-sblue font-mono mb-5"
                 id="exampleModalLabel"
               >
                 Register
               </h5>
-              <button className="btn btn-dark w-100 mb-4">
-                <span className="fa fa-google me-2"></span> Sign in With Google
+              <button className="btn bg-sblue text-lg  flex justify-center items-center  text-white w-full mb-4">
+                    <BsGoogle className="block m-1 h-5 w-5" aria-hidden="true" />
+                 <span className="ml-2">
+                    Sign up with Google
+                 </span> 
               </button>
-              <form onSubmit={register}>
-                <div className="mb-3 text-start">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
+              <form className="flex flex-col"
+              onSubmit={register}>
+                <div className="mb-2 ml-1 flex flex-col">
+                  <label htmlFor="exampleInputEmail1"
+                   className=" text-lg font-normal leading-relaxed mb-2">
                     Username
                   </label>
                   <input
                     type="text"
-                    className="form-control"
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leasding-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     required
@@ -78,13 +81,16 @@ function Signup() {
                     onChange={(e) => setName(e.target.value)}
                   />
                 </div>
-                <div className="mb-3 text-start">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
+                <div className="mb-3 flex flex-col">
+                  <label htmlFor="exampleInputEmail1" 
+                  
+                  className="text-lg font-normal leading-relaxed mb-2"
+                  >
                     Email address
                   </label>
                   <input
                     type="email"
-                    className="form-control"
+                    className="formstyle mb-2"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     required
@@ -95,20 +101,22 @@ function Signup() {
                     We'll never share your email with anyone else.
                   </div>
                 </div>
-                <div className="mb-3 text-start">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
+                <div className="mb-3 flex flex-col text-start">
+                  <label htmlFor="exampleInputPassword1" 
+                  className="text-lg font-normal leading-relaxed mb-2"
+                  >
                     Password
                   </label>
                   <input
                     required
                     type="password"
-                    className="form-control"
+                    className="formstyle"
                     id="exampleInputPassword1"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
-                <div className="mb-3 form-check text-start">
+                <div className="mb-3 flex justify-around items-center">
                   <input
                     type="checkbox"
                     className="form-check-input"
@@ -119,7 +127,7 @@ function Signup() {
                     I agree the terms and conditions
                   </label>
                 </div>
-                <button type="submit" className="btn btn-dark w-100 mt-5 mb-3">
+                <button type="submit" className="btn transbtn w-100 mt-5 mb-3">
                   Register
                 </button>
               </form>
@@ -127,7 +135,8 @@ function Signup() {
           </div>
         </>
       )}
-    </>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          swhite: "#E5E5E5",
+          sblue: "rgba(44,82,106,.934)"
+        }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
